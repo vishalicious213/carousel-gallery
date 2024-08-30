0f60f6313dc165878bc7e987b021b3c2d1d83b2b
vishalicious213 <vishalicious213@gmail.com>
2024-08-30 13:58:25 -0700

    added kittyArr & puppyArr

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const kittyBtn = document.getElementById("kitty-btn")
 const puppyBtn = document.getElementById("puppy-btn")
 
+const kittyArr = ["kitty-1.jpg", "kitty-2.jpg", "kitty-3.jpg"]
+const puppyArr = ["puppy-1.jpg", "puppy-2.jpg", "puppy-3.jpg"]
+
+// ⬇️ EVENT LISTENERS ⬇️
+
 kittyBtn.addEventListener("click", kittyClick)
 puppyBtn.addEventListener("click", puppyClick)
+
+// ⬇️ EVENT HANDLERS ⬇️
 
 function kittyClick() {
     console.log("kitty button clicked")
@@ -11,3 +18,9 @@ function kittyClick() {
 function puppyClick() {
     console.log("puppy button clicked")
 }
+
+// ⬇️ RENDER FUNCTIONS ⬇️
+
+// render carousel
+
+// render gallery
