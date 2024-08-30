9384cd9e81502ba1187d2eaa19931be7c092c83f
vishalicious213 <vishalicious213@gmail.com>
2024-08-30 13:51:40 -0700

    grabbed buttons & added event listeners

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("TEST")
+const kittyBtn = document.getElementById("kitty-btn")
+const puppyBtn = document.getElementById("puppy-btn")
+
+kittyBtn.addEventListener("click", kittyClick)
+puppyBtn.addEventListener("click", puppyClick)
+
+function kittyClick() {
+    console.log("kitty button clicked")
+}
+
+function puppyClick() {
+    console.log("puppy button clicked")
+}
