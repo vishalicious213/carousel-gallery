c7e2487a9b9a2f508cd691cd0754e6d0d717d33e
vishalicious213 <vishalicious213@gmail.com>
2024-08-31 20:23:37 -0700

    showing first critter in htmlCollection when button is clicked in carousel

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const puppyBtn = document.getElementById("puppy-btn")
 const carousel = document.getElementById("carousel-container")
 const carouselImg = document.getElementById("carousel-images")
 const gallery = document.getElementById("gallery-container")
-let slideIndex = 1
+let slideIndex = 0
 
 const kittyArr = ["kitty-1.jpg", "kitty-2.jpg", "kitty-3.jpg"]
 const puppyArr = ["puppy-1.jpg", "puppy-2.jpg", "puppy-3.jpg"]
@@ -34,17 +34,23 @@ function changeSlide(n) {
 // ⬇️ RENDER FUNCTIONS ⬇️
 
 // render carousel
-function renderCarousel(arr) {
-    console.log(arr)
+function renderCarousel(arr, slideNum = 0) {
+    console.log(arr, slideNum)
     carouselImg.innerHTML = ""
 
     const critters = arr.map(item => `
-        <section class="carousel-critter">
+        <section class="carousel-critter hidden">
             <img src="img/${item}" alt="">
         </section>
-    `)
+    `).join("")
 
     carouselImg.innerHTML += critters
+
+    let critterToShow = document.getElementsByClassName("carousel-critter")[slideNum]
+    critterToShow.className = "carousel-critter"
+    console.log(critterToShow)
+
+    carouselImg.innerHTML += critterToShow
 }
 
 // render gallery
