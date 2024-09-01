45ed2f1d781842a0317e66c7f4422a665ba8b775
vishalicious213 <vishalicious213@gmail.com>
2024-08-31 18:21:57 -0700

    set up changeSlide() and added it as onclick for prev/next buttons

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const puppyBtn = document.getElementById("puppy-btn")
 const carousel = document.getElementById("carousel-container")
 const carouselImg = document.getElementById("carousel-images")
 const gallery = document.getElementById("gallery-container")
+let slideIndex = 1
 
 const kittyArr = ["kitty-1.jpg", "kitty-2.jpg", "kitty-3.jpg"]
 const puppyArr = ["puppy-1.jpg", "puppy-2.jpg", "puppy-3.jpg"]
@@ -24,6 +25,10 @@ function puppyClick() {
     console.log("puppy button clicked")
     renderCarousel(puppyArr)
     renderGallery(puppyArr)
+}
+
+function changeSlide(n) {
+    console.log("change slide", n)
 }
 
 // ⬇️ RENDER FUNCTIONS ⬇️
